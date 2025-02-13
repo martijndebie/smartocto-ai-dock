0b2be3bfe7a97c5dcc3da545066ee9a1c9f99ffe
Martijn de Bie <martijndebie@Occies-MacBook-Air.local>
2025-02-13 22:53:29 +0100

    Updated scripts to load dock and send information to API

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 document.getElementById('injectButton').addEventListener('click', async () => {
+  
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['contentScript.js']
   });
+  window.close();
 });
